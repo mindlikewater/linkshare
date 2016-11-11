e73ea184b9df1f8b24e569f596eba953f053470f
Tai Chen <mindlikewater@gmail.com>
2016-11-11 16:44:26 -0500

    Added routes for creating new users and for users to log in

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -18,3 +18,7 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
+
+Route.post('/users', 'UserController.register')
+Route.post('/login', 'UserController.login')
+Route.get('/auth_test', 'UserController.auth_test').middleware('auth')
