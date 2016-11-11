abb6679b595c42926118838f58de8a12d39ef836
Tai Chen <mindlikewater@gmail.com>
2016-11-11 17:17:53 -0500

    Added methods for registering users, logging in, and testing authentication of a user

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -1,7 +1,42 @@
 'use strict'
 
-class UserController {
+const Hash = use('Hash')
+const User = use('App/Model/User')
 
+class UserController {
+  * register (request, response) {
+    let data = request.only('username', 'email', 'password')
+    data.password = yield Hash.make(data.password)
+    let user = yield User.create(data)
+
+    response.status(201).json(user)
+  }
+
+  * login (request, response) {
+    let data = request.only('username', 'password')
+    let user = yield User.findBy('username', data.username)
+
+    try {
+      //Note: If user is nul, this TypeErrors, which is caught below.
+      let verify = yield Hash.verify(data.password, user.password)
+      if (!verify) { throw new Error(); }
+
+      let token = yield request.auth.generate(user)
+      user.access_token = Token
+
+      response.json(user)
+    }
+    catch (e) {
+      response.status(401).json({ error: "No such user or password"})
+    }
+  }
+
+  * auth_test (request, response) {
+    let user = request.authUser
+
+    console.log(request.authUser)
+    response.json(user)
+  }
 }
 
 module.exports = UserController
