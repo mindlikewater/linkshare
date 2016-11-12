c835fc5acc1a8d357a589bd1591b9a07f51e7896
Tai Chen <mindlikewater@gmail.com>
2016-11-12 13:48:23 -0500

    Added routes for links-aka. topics, and for comments

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -21,3 +21,12 @@ Route.on('/').render('welcome')
 
 Route.post('/users', 'UserController.register')
 Route.post('/login', 'UserController.login')
+//Route.get('/auth_test', 'UserController.auth_test').middleware('auth')
+
+Route.post('/topics', 'TopicController.create').middleware('auth')
+Route.get('/topics', 'TopicController.index')
+Route.delete('/topics/:topic_id', 'TopicController.delete').middleware('auth')
+
+Route.post('/comments', 'CommentController.create').middleware('auth')
+Route.get('/topics/:topic_id/comments', 'CommentController.index')
+Route.delete('/topics/:topic_id/comments/:comment_id', 'CommentController.delete').middleware('auth')
