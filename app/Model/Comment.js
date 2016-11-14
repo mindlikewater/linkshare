82be2a9b8386263a526172f861194ee40354c8eb
Tai Chen <mindlikewater@gmail.com>
2016-11-13 21:12:41 -0500

    Added association of many comments to a topic

--- app/Model/Comment.js
+++ app/Model/Comment.js
@@ -3,7 +3,7 @@
 const Lucid = use('Lucid')
 
 class Comment extends Lucid {
-
+  return this.belongsTo('App/Model/Topic')
 }
 
 module.exports = Comment
--- app/Model/Topic.js
+++ app/Model/Topic.js
@@ -3,7 +3,12 @@
 const Lucid = use('Lucid')
 
 class Topic extends Lucid {
-  return this.belongsTo('App/Model/User')
+  users () {
+    return this.belongsTo('App/Model/User')
+  }
+  comments () {
+    return this.hasMany('App/Model/Comment')
+  }
 }
 
 module.exports = Topic
