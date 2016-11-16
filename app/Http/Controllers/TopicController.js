b24245a83d74acf9279470f550e98f1fe24fdc5f
Tai Chen <mindlikewater@gmail.com>
2016-11-16 16:29:04 -0500

    Got delete to work

--- app/Http/Controllers/TopicController.js
+++ app/Http/Controllers/TopicController.js
@@ -4,29 +4,33 @@ const Topic = use('App/Model/Topic')
 
 class TopicController {
   * create (request, response) {
+      let user = request.authUser
       let data = request.only('title', 'url')
-      let topic = yield Topic.create(data)
+      data.user_id = user.id
+      // let topic = yield Topic.create(data)
+      let topic = new Topic(data)
+      yield user.topics().save(topic)
 
       response.status(201).json(topic)
   }
 
   * index (request, response) {
-    let topics = yield Topic.query().select('title', 'url').orderBy('updated_at', 'desc')
+    let topics = yield Topic.query().select("*").orderBy('updated_at', 'desc')
 
     response.json(topics)
   }
 
   * delete (request, response) {
-    let userId = request.param('user_id')
-    let topics = yield Topic.findBy('user_id', userId)
-    let eraseTopic = yield Topic.query().where(userId, 'topics')
+    let user = request.auth.getUser()
+    let topicId = request.param('topic_id')
+    let topic = yield Topic.query().where('id', topicId)
 
-    if (!eraseTopic) {
-      response.status(404).json({ error: "Topic not found" })
+    if (user.id === topic.user_id) {
+      topic = yield Topic.query().where('id', topicId).del()
+      response.status(204).json({ success: "Topic successfully deleted" })
     }
     else {
-      eraseTopic = yield Topic.query().where(userId, 'topics').del()
-      response.status(204).json({ success: "Topic successfully deleted" })
+      response.status(404).json({ error: "Topic not found" })
     }
   }
 }
